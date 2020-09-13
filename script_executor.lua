87d234595f472ea7713c459ded6672da9f881291
thedoomed <46077863+thedoomed@users.noreply.github.com>
2020-09-13 13:24:05 -0400

    Update script_executor.lua

--- script_executor.lua
+++ script_executor.lua
@@ -1,5 +1,5 @@
 -- open the current roblox process
--- 
+--
 local pid = getProcessIDFromProcessName("RobloxPlayerBeta.exe");
 openProcess(pid);
 local base = getAddress(enumModules(pid)[1].Name);
@@ -21,7 +21,7 @@ function writeByte(addr, b)
     writeBytes(addr, { b });
 end
 
-function byte_to_str(b)
+function to_str(b)
     if b == nil then return "00" end
     local str="";
     if b <= 256 then
@@ -37,7 +37,7 @@ function addr_to_bytes(addr)
     end
     local bytes = {0,0,0,0};
     for i=0,3 do
-        bytes[4-i]=(addr>>(i*8))%256;
+        bytes[3-i]=(addr>>(i*8))%256;
     end
     return bytes;
 end
@@ -48,8 +48,8 @@ function addr_to_str(addr)
     end
     local str="";
     local bytes = addr_to_bytes(addr);
-    for i=1,4 do -- lua tables
-        str = str..byte_to_str(bytes[i])
+    for i=0,3 do -- lua tables
+        str = str..to_str(bytes[i])
     end
     return str;
 end
@@ -73,12 +73,12 @@ end
 function readsb(addr, len)
     local str = "";
     for i=1,len do
-        str=str..byte_to_str(readByte(addr));
+        str=str..to_str(readByte(addr));
     end
     return str;
 end
 
-function getprologue(addr)
+function getPrologue(addr)
     local func_start = addr;
     while not (readByte(func_start) == 0x55 and readByte(func_start + 1) == 0x8B and readByte(func_start + 2) == 0xEC) do
         func_start = func_start - 1;
@@ -86,7 +86,7 @@ function getprologue(addr)
     return func_start;
 end
 
-function getnextprologue(addr)
+function getNextPrologue(addr)
     local func_start = addr;
     while not (readByte(func_start) == 0x55 and readByte(func_start + 1) == 0x8B and readByte(func_start + 2) == 0xEC) do
         func_start = func_start + 1;
@@ -94,34 +94,30 @@ function getnextprologue(addr)
     return func_start;
 end
 
-function memcpy(t, addr, size)
-    if (type(t) == "table") then
-        for i=1,size do
-            t[i] = readByte(addr + (i - 0));
-        end
-    elseif (type(t) == "number") then
-        writeBytes(t, readBytes(addr, size, true));
-    end
-end
 
 
-
+-- Search for an XREF of the string
+-- "Spawn function requires 1 argument"...
+-- Scanning the string's address will take us
+-- to the spawn function
+local str_spawn_ref = addr_to_bytes(getAddress(AOBScan("537061776E20","-C-W",0,"")[0])); -- address of the string "Spawn " . . .
+local str_spawn_bytes = to_str(str_spawn_ref[3])..to_str(str_spawn_ref[2])..to_str(str_spawn_ref[1])..to_str(str_spawn_ref[0]);
 
 -- scan for our lua functions
+local r_spawn       = getAddress(AOBScan(str_spawn_bytes,"-C-W",0,"")[0]);
 local r_deserialize = getAddress(AOBScan("0F????83??7FD3??83??0709","-C-W",0,"")[0]);
-local r_spawn       = getAddress(AOBScan("83????F20F10????F20F??????FF75","-C-W",0,"")[0]);
 local r_gettop      = getAddress(AOBScan("558BEC8B??088B????2B??????????5DC3","-C-W",0,"")[0]);
 local r_gettable;
 
-r_deserialize      	= getprologue(r_deserialize);
-r_spawn            	= getprologue(r_spawn);
-r_gettable         	= getprologue(r_gettop - 3);
+r_deserialize      = getPrologue(r_deserialize);
+r_spawn            = getPrologue(r_spawn);
+r_gettable         = getPrologue(r_gettop - 1);
 
 -- these calling conventions will certainly never change
 -- but you can manually input them here
--- 
-conv_deserialize   	= "cdecl";
-conv_spawn         	= "cdecl";
+--
+conv_deserialize   = "cdecl";
+conv_spawn         = "cdecl";
 
 -- a place to store our function information
 -- for external function calls
@@ -129,6 +125,8 @@ conv_spawn         	= "cdecl";
 local arg_data = allocateSharedMemory("arg_data", 0x1000);
 if arg_data == nil then
     error'Failed to allocate shared memory...'
+else
+    print("lua_gettop hook: " .. addr_to_str(arg_data + 0x400))
 end
 
 local ret_location = (arg_data + 64);
@@ -146,63 +144,59 @@ local gettop_old_bytes = readBytes(r_gettop + 6, 6, true);
 -- to place our state hooking code at
 local gettop_hook_loc = arg_data + 0x400;
 local trace_loc = arg_data + 0x3FC;
+local jmp_pointer_to = arg_data + 0x3F8;
+local jmp_pointer_back = arg_data + 0x3F4;
+bytes_jmp = addr_to_bytes(jmp_pointer_back);
 
--- make the hook
-writeByte(gettop_hook_loc, 0x60);
-writeBytes(gettop_hook_loc + 1, { 0x89, 0x0D });
-writeInteger(gettop_hook_loc + 3, trace_loc);
-writeByte(gettop_hook_loc + 7, 0x61);
-writeBytes(gettop_hook_loc + 8, gettop_old_bytes);
-writeByte(gettop_hook_loc + 14, 0xE9);
-writeInteger(gettop_hook_loc + 15, (r_gettop + 6 + 6) - (gettop_hook_loc + 14 + 5));
+writeInteger(jmp_pointer_to, gettop_hook_loc);
+writeInteger(jmp_pointer_back, r_gettop);
 
--- get a jmp instruction prepared
-local gettop_rel = gettop_hook_loc - (r_gettop + 6 + 5);
-local gettop_rel_bytes = addr_to_bytes(gettop_rel);
-local gettop_hook = { 0xE9, 0x90, 0x90, 0x90, 0x90, 0x90 };
-gettop_hook[2] = gettop_rel_bytes[4];
-gettop_hook[3] = gettop_rel_bytes[3];
-gettop_hook[4] = gettop_rel_bytes[2];
-gettop_hook[5] = gettop_rel_bytes[1];
+-- append bytes to the hook (this is sloppy xd)
+writeBytes(gettop_hook_loc,             { 0x60, 0x89, 0x0D });
+writeInteger(gettop_hook_loc + 3,       trace_loc);
+writeByte(gettop_hook_loc + 7, 	        0x61);
+writeBytes(gettop_hook_loc + 8,         gettop_old_bytes);
+writeByte(gettop_hook_loc + 14, 	{ 0xFF, 0x25, bytes_jmp[3], bytes_jmp[2], bytes_jmp[1], bytes_jmp[0] });
+
+-- insert a jmp instruction
+bytes_jmp = addr_to_bytes(jmp_pointer_to);
+local gettop_hook = { 0xFF, 0x25, bytes_jmp[3], bytes_jmp[2], bytes_jmp[1], bytes_jmp[0] };
 
 
 
 -- handle the external calling convention routines...
--- aka convert every function into an stdcall if it's not
--- already.
--- 
+-- convert every function into an stdcall if it's not
+-- already
+--
 function make_stdcall(func, convention, args)
     if (convention == "stdcall") then
         return func
     end
-    
+
     local ret = args * 4;
     nfunctions = nfunctions + 1;
     local loc = allocateSharedMemory("func"..tostring(nfunctions), 4096)
-    
+
     local code = "";
     code = code .. addr_to_str(loc)..": \n";
     code = code .. "push ebp \n";
     code = code .. "mov ebp,esp \n";
     code = code .. "push eax \n";
-    
+
     if (convention == "cdecl") then
         for i=args,1,-1 do
             -- since cheat engine's executeCode (a.k.a. CreateRemoteThread)
             -- can only pass 1 arg to a function, we can compensate
-            -- by passing all of our beloved args through variables in memory.
-            -- This method is even more efficient, and if we wanted we can
-            -- set this thing up to handle all function calls on a single thread
-            -- (which i've done in C++ already)
-            -- For now, we can spawn a thread for each function call as this
-            -- literally only needs 2 function calls to work.
+            -- by passing all of our args through variables in memory.
+            -- We can spawn a thread for each function call as this
+            -- only needs 2 function calls to work.
             --
-            code = code .. "push ["..addr_to_str(arg_data+((i-1)*4)).."] \n" --"push "..args--"push [ebp+"..byte_to_str(4+(i*4)).."] \n";
+            code = code .. "push ["..addr_to_str(arg_data+((i-1)*4)).."] \n" --"push "..args--"push [ebp+"..to_str(4+(i*4)).."] \n";
 	end
     elseif (convention == "thiscall") then
         if (args > 1) then
             for i=args,2,-1 do
-                code = code .. "push ["..addr_to_str(arg_data+((i-1)*4)).."] \n" --"push [ebp+"..byte_to_str(4+(i*4)).."] \n";
+                code = code .. "push ["..addr_to_str(arg_data+((i-1)*4)).."] \n" --"push [ebp+"..to_str(4+(i*4)).."] \n";
 	    end
         end
         if (args > 0) then
@@ -213,7 +207,7 @@ function make_stdcall(func, convention, args)
     elseif (convention == "fastcall") then
        	if (args > 2) then
             for i=args,3,-1 do
-                code = code .. "push ["..addr_to_str(arg_data+((i-1)*4)).."] \n" --"push [ebp+"..byte_to_str(4+(i*4)).."] \n";
+                code = code .. "push ["..addr_to_str(arg_data+((i-1)*4)).."] \n" --"push [ebp+"..to_str(4+(i*4)).."] \n";
 	    end
         end
         if (args > 0) then
@@ -227,52 +221,52 @@ function make_stdcall(func, convention, args)
             ret = ret - 4;
         end
     end
-    
+
     code = code .. "call "..addr_to_str(func).." \n"
     code = code .. "mov ["..addr_to_str(arg_data + 64).."],eax \n";
-    
+
     if (convention == "cdecl") then
-        code = code .. "add esp,"..byte_to_str(args*4).." \n"
+        code = code .. "add esp,"..to_str(args*4).." \n"
     elseif (convention == "thiscall") then
         code = code .. "pop ecx \n"
     elseif (convention == "fastcall") then
         code = code .. "pop ecx \n"
         code = code .. "pop edx \n"
     end
-    
+
     code = code .. "pop eax \n";
     code = code .. "pop ebp \n"
     code = code .. "ret 04";
-    --code = code .. "ret " .. byte_to_str(ret) .. " \n"
-    
+    --code = code .. "ret " .. to_str(ret) .. " \n" -- use to convert to cdecl
+
     autoAssemble(code);
     return loc;
 end
 
 function patch_retcheck(func_start)
-    local func_end = getnextprologue(func_start + 3);
+    local func_end = getNextPrologue(func_start + 3);
     local func_size = func_end - func_start;
-	
+
     nfunctions = nfunctions + 1;
     local func = allocateSharedMemory("func"..tostring(nfunctions), func_size);
-    memcpy(func, func_start, func_size); -- copy function here
-	
-	for i = 1,func_size,1 do
-		local at = func + i;
-		if (readByte(at) == 0x72 and readByte(at + 2) == 0xA1 and readByte(at + 7) == 0x8B) then
-			writeByte(at, 0xEB);
-			print("Patched retcheck at "..addr_to_str(at))
-			break;
-		end
-	end
-		
+    writeBytes(func, readBytes(func_start, func_size, true));
+
+    for i = 1,func_size,1 do
+        local at = func + i;
+        if (readByte(at) == 0x72 and readByte(at + 2) == 0xA1 and readByte(at + 7) == 0x8B) then
+            writeByte(at, 0xEB);
+            print("Patched retcheck at "..addr_to_str(at))
+            break;
+        end
+    end
+
     local i = 1;
     while (i < func_size) do
         -- Fix relative calls
         if (readByte(func + i) == 0xE8 or readByte(func + i) == 0xE9) then
             local oldrel = readInteger(func_start + i + 1);
             local relfunc = (func_start + i + oldrel) + 5;
-                
+
             if (relfunc % 16 == 0 and relfunc > base and relfunc < base + 0x3FFFFFF) then
                 local newrel = relfunc - (func + i + 5);
                 writeInteger((func + i + 1), newrel);
@@ -305,9 +299,11 @@ print("");
 -- and bypass retcheck if there is a retcheck
 r_deserialize = make_stdcall(r_deserialize, conv_deserialize, 4);
 r_spawn = make_stdcall(r_spawn, conv_spawn, 1);
+r_newthread = patch_retcheck(0x121F7D0 + base - 0x400000);
 
 print("r_deserialize: "..addr_to_str(r_deserialize));
 print("r_spawn: "..addr_to_str(r_spawn));
+print("r_newthread: "..addr_to_str(r_newthread));
 
 local url = "https://raw.githubusercontent.com/thedoomed/Cheat-Engine/master/bytecode_example.bin"
 local http = getInternet()
@@ -326,7 +322,7 @@ writeInteger(bytecode_loc + bytecode_size + (bytecode_size + 4 % 4), bytecode_si
 print(addr_to_str(bytecode_loc));
 
 local chunkName = (arg_data + 128);
-writeString(chunkName, "@Script1");
+writeString(chunkName, "=Script1");
 writeInteger(chunkName + 12, 8); -- string length
 
 -- place the hook for gettop
@@ -343,16 +339,17 @@ function checkHook(timer)
             -- restore bytes
             writeBytes(r_gettop + 6, gettop_old_bytes);
             timer_setEnabled(t, false);
-            
+
             print("Lua state: " ..addr_to_str(rL));
-            
+
             setargs({rL, chunkName, bytecode_loc, bytecode_size});
-            executeCode(r_deserialize);
-            executeCode(r_spawn); -- use 1st arg
+            print("Args location: " .. addr_to_str(arg_data));
+            executeCode(r_deserialize, 0, 250);
+            executeCode(r_spawn, 0, 250); -- uses rL from last setargs call ...
         end
     end
 end
 
-timer_setInterval(t, 10);
+timer_setInterval(t, 100);
 timer_onTimer(t, checkHook);
 timer_setEnabled(t, true);
